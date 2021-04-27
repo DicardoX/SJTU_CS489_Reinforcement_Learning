eb45ca635d111d5291bcb79dede37931ee549a42
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2021-04-27 17:51:43 +0800

    Update

--- Project4/MountainCar_v0.py
+++ Project4/MountainCar_v0.py
@@ -3,7 +3,7 @@
 import gym
 import numpy as np
 from matplotlib import pyplot as plt
-from RL_brain import DeepQNetwork
+from RL_brain import DeepQNetwork, Double_DeepQNetwork
 import time
 import os
 
@@ -13,9 +13,17 @@ env = gym.make("MountainCar-v0")
 env = env.unwrapped
 
 # Instantiation of DQN
-model = DeepQNetwork(n_actions=3, n_features=2, neurons_num=16, learning_rate=0.0001, epsilon_greedy=0.9, replace_target_iter=300,
-                     buffer_size=5000, epsilon_greedy_increment=0.0005)
+DQN_model = DeepQNetwork(n_actions=3, n_features=2, neurons_num=16, learning_rate=0.0001, epsilon_greedy=0.9,
+                         replace_target_iter=300,
+                         buffer_size=5000, epsilon_greedy_increment=0.0005)
+# Instantiation of Double_DQN
+Double_DQN_model = Double_DeepQNetwork(n_actions=3, n_features=2, neurons_num=16, learning_rate=0.0001,
+                                       epsilon_greedy=0.9,
+                                       replace_target_iter=300,
+                                       buffer_size=5000, epsilon_greedy_increment=0.0005)
 
+# Choose use DQN or DDQN: 0 for DQN, 1 for DDQN
+choice_index = 1
 
 # Step list
 step_list = []
@@ -29,17 +37,19 @@ episode_time_list = []
 message_list = []
 
 # Write path
-write_path = "./output/console_log.txt"
+write_path = "./output/console_log.txt" if choice_index == 0 else "./output2/console_log_for_DDQN.txt"
 
 
 # Plot
 def plot_info():
+    output_path = "./output/" if choice_index == 0 else "./output2/"
+
     # Plot step list
     step_list_x = np.arange(len(step_list))
     plt.plot(step_list_x, step_list)
     plt.xlabel("Epochs")
     plt.ylabel("Step of episode")
-    plt.savefig("./output/step_list")
+    plt.savefig(output_path + "step_list")
     plt.show()
 
     # Plot average action value
@@ -47,7 +57,7 @@ def plot_info():
     plt.plot(action_value_x, action_value)
     plt.xlabel("Epochs")
     plt.ylabel("Average action value")
-    plt.savefig("./output/action_value")
+    plt.savefig(output_path + "action_value")
     plt.show()
 
     # Plot episode cost list
@@ -55,7 +65,7 @@ def plot_info():
     plt.plot(episode_cost_list_x, episode_cost_list)
     plt.xlabel("Epochs")
     plt.ylabel("Episode cost")
-    plt.savefig("./output/episode_cost_list")
+    plt.savefig(output_path + "episode_cost_list")
     plt.show()
 
     # Plot episode time list
@@ -63,11 +73,14 @@ def plot_info():
     plt.plot(episode_time_list_x, episode_time_list)
     plt.xlabel("Epochs")
     plt.ylabel("Episode time")
-    plt.savefig("./output/episode_time_list")
+    plt.savefig(output_path + "episode_time_list")
     plt.show()
 
     # Plot step cost
-    model.plot_cost_curve()
+    if choice_index == 0:
+        DQN_model.plot_cost_curve()
+    else:
+        Double_DQN_model.plot_cost_curve()
 
 
 if __name__ == '__main__':
@@ -77,6 +90,10 @@ if __name__ == '__main__':
     # Step counter
     steps_counter = 0
 
+    print("--------------------------------------------")
+    print("Begin", "DQN" if choice_index == 0 else "Double DQN", "training...")
+    print("--------------------------------------------")
+
     for i in range(total_episodes):
         # Observation
         observation = env.reset()
@@ -85,7 +102,10 @@ if __name__ == '__main__':
         tmp_steps = 0
 
         # Clear self.episode_cost
-        model.episode_cost = 0
+        if choice_index == 0:
+            DQN_model.episode_cost = 0
+        else:
+            Double_DQN_model.episode_cost = 0
 
         # Time mark
         time_mark = time.time()
@@ -94,7 +114,9 @@ if __name__ == '__main__':
             # Render the environment
             env.render()
             # Choose action
-            action = model.choose_action(observation=observation)
+            action = DQN_model.choose_action(
+                observation=observation) if choice_index == 0 else Double_DQN_model.choose_action(
+                observation=observation)
             # Get feedback from environment
             new_observation, reward, done, info = env.step(action)
             # Parse the observation
@@ -103,19 +125,27 @@ if __name__ == '__main__':
             reward = abs(position - (-0.5))
 
             # Store the transition
-            model.store_transition(observation, action, reward, new_observation)
+            if choice_index == 0:
+                DQN_model.store_transition(observation, action, reward, new_observation)
+            else:
+                Double_DQN_model.store_transition(observation, action, reward, new_observation)
 
             # Learn
             if steps_counter > 1000:
-                model.learn()
+                if choice_index == 0:
+                    DQN_model.learn()
+                else:
+                    Double_DQN_model.learn()
             # Update reward
             episode_reward += reward
 
             # If done
             if done:
                 sub_message = "| Get" if new_observation[0] >= env.unwrapped.goal_position else "| ----"
-                message = "Episode: " + str(i) + " " + sub_message + " | Episode reward: " + str(round(episode_reward, 3)) + \
-                          " | Episode steps: " + str(tmp_steps) + " | Episode cost: " + str(round(model.episode_cost, 3)) + \
+                message = "Episode: " + str(i) + " " + sub_message + " | Episode reward: " + str(
+                    round(episode_reward, 3)) + \
+                          " | Episode steps: " + str(tmp_steps) + " | Episode cost: " + str(
+                    round(DQN_model.episode_cost if choice_index == 0 else Double_DQN_model.episode_cost, 3)) + \
                           " | Episode time: " + str(round(time.time() - time_mark, 3)) + " seconds"
                 print(message)
                 # Update message list
@@ -133,7 +163,7 @@ if __name__ == '__main__':
         # Update steps list
         step_list.append(tmp_steps)
         # Update episode cost list
-        episode_cost_list.append(model.episode_cost)
+        episode_cost_list.append(DQN_model.episode_cost if choice_index == 0 else Double_DQN_model.episode_cost)
         # Update episode time list
         episode_time_list.append(round(time.time() - time_mark))
 
@@ -149,6 +179,3 @@ if __name__ == '__main__':
 
     # Plot
     plot_info()
-
-
-
---
+++ Project4/README.md
@@ -0,0 +1,4 @@
+# Deep Q-learning Network & Double Deep Q-learning Network
+
+[强化学习：DQN与Double DQN讨论](http://www.broadview.com.cn/article/419382)
+
